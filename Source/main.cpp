d5728967f206279b068323c8c36c9f29ce78bdcc
Коптяев Александр <a.koptyaev@playrix.com>
2024-08-27 03:31:07 +0600

    Прототип vector

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
+#include "Examples/HeadersExample.h"
 
 int main() {
 	std::cout << "START" << std::endl;
-	
+
+	VectorExample();
+
 	char chars[32];
 	std::cin >> chars;
 	std::cout << "END: " << chars << std::endl;
